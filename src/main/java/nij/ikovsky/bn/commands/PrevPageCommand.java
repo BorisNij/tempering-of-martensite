88dd41fe9a3a987b105330ae38b8adf0959c9905
bn <bn@bn.bn>
2020-12-09 20:48:48 -0500

    debug prev()

--- src/main/java/nij/ikovsky/bn/commands/PrevPageCommand.java
+++ src/main/java/nij/ikovsky/bn/commands/PrevPageCommand.java
@@ -6,22 +6,29 @@ import nij.ikovsky.bn.UserConsole;
 
 import java.util.Map;
 
-@SuppressWarnings("rawtypes")
 public class PrevPageCommand implements SpotifyExplorerCommand {
     private final AuthService authService;
     private final UserConsole view;
+    @SuppressWarnings("rawtypes")
     private final Map<String, Cache> itemCaches;
 
-    public PrevPageCommand(AuthService authService, UserConsole view, Map<String, Cache> itemCaches) {
+    public PrevPageCommand(AuthService authService, UserConsole view, @SuppressWarnings("rawtypes") Map<String, Cache> itemCaches) {
         this.authService = authService;
         this.view = view;
         this.itemCaches = itemCaches;
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void execute() {
         if (this.authService.isAuthorized()) {
+            @SuppressWarnings("rawtypes") final Cache nowShowing = this.itemCaches.get("nowShowing");
+            if (nowShowing.getItems()
+                    .isEmpty()) {
+                this.view.errorMsg("Item list is not initialized. " +
+                                   "Try fetching items from Spotify first ('new', 'featured', 'categories')");
+                return;
+            }
+            //noinspection unchecked
             this.view.display(this.itemCaches.get("nowShowing")
                                       .prevPage());
         } else {
